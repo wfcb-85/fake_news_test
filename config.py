ca484333c1973d5db3b1977b3bc213fadc0325ac
William <williamcorrea85@gmail.com>
2022-07-10 15:07:15 -0400

    added a config file

---
+++ config.py
@@ -0,0 +1,6 @@
+params = {
+'lr':5e-5,
+'n_epochs':1,
+'batch_size':16,
+
+}
--- dataset_plus_bert.py
+++ dataset_plus_bert.py
@@ -5,16 +5,15 @@ https://huggingface.co/docs/transformers/model_doc/distilbert
 
 """
 import pdb
-from transformers import DistilBertTokenizerFast
-tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
-from datasets import load_dataset
 import torch
-from transformers import DistilBertForSequenceClassification, Trainer, TrainingArguments
+from transformers import DistilBertForSequenceClassification
 from torch.utils.data import DataLoader
 from transformers import DistilBertForSequenceClassification, AdamW
 
 from prepare_dataset import get_datasets
 from evaluation import evaluate_pytorch_model
+
+from config import params
 
 train_dataset, val_dataset,_,val_texts, train_labels,val_labels = get_datasets()
 
@@ -25,12 +24,11 @@ model.to(device)
 
 model.train()
 
-train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=params['batch_size'], shuffle=True)
 
-optim = AdamW(model.parameters(), lr=5e-5)
+optim = AdamW(model.parameters(), lr=params['lr'])
 
-n_epochs = 1
-for epoch in range(n_epochs):
+for epoch in range(params['n_epochs']):
     for batch in train_loader:
         optim.zero_grad()
         input_ids = batch['input_ids'].to(device)
